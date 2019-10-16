960e9cc436649d0437dfd6f10b80bbbfc08f4f4c
Mert Oğuz <mertoguz94@gmail.com>
2019-10-16 21:07:52 +0200

    indent by space instead of tabs

--- src/todo-form.js
+++ src/todo-form.js
@@ -2,28 +2,28 @@ import React from 'react';
 
 export class TodoForm extends React.Component {
   
-	constructor() {
-		super();
-		this.addToList = this.addToList.bind(this);
-	}
-		
-	addToList (e) {
-		e.preventDefault();
-		const input = document.getElementById('add-todo');
-		this.props.addToList(input.value);
-		input.value = '';
-	}
+    constructor() {
+        super();
+        this.addToList = this.addToList.bind(this);
+    }
+        
+    addToList (e) {
+        e.preventDefault();
+        const input = document.getElementById('add-todo');
+        this.props.addToList(input.value);
+        input.value = '';
+    }
 
-	render () {
-		return (
-			<div className="todo-form">
-				<form className="form-inline" onSubmit={this.addToList} >
-					<div className="add-todo">
-					<input id="add-todo" type="text" autoComplete="off" className="form-control" placeholder="What else to do.."/>
-					<button className="btn btn-outline-primary" type="button" onClick={this.addToList}>Add to list</button>
-					</div>
-				</form>
-			</div>
-		)
-	}
+    render () {
+        return (
+            <div className="todo-form">
+                <form className="form-inline" onSubmit={this.addToList} >
+                    <div className="add-todo">
+                    <input id="add-todo" type="text" autoComplete="off" className="form-control" placeholder="What else to do.."/>
+                    <button className="btn btn-outline-primary" type="button" onClick={this.addToList}>Add to list</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
--- src/todo-list.js
+++ src/todo-list.js
@@ -1,49 +1,49 @@
 import React from 'react';
 
 export class TodoList extends React.Component {
-	setToDone (taskId) {
-		this.props.setToDone(taskId);
-	}
+    setToDone (taskId) {
+        this.props.setToDone(taskId);
+    }
 
-	remove (taskId) {
-		this.props.remove(taskId);
-	}
+    remove (taskId) {
+        this.props.remove(taskId);
+    }
 
-	render () {
-		const items = this.props.thingsToDo.map((el, i) => {
-			let taskId = "task" + i;
-			return (
-				<li key={i} id={taskId} className="list-group-item todo-item">
-					<div className="todo-task">
-						<span className="id">{i+1}. </span>
-						<span className="title">{el.text}</span>
-						<span className={`todo-${el.status} ml-2`}><i className="fas fa-check"/></span>
-					</div>
-					<div className="todo-buttons" id={taskId}>
-						<button
-							type="button" 
-							className="btn btn-outline-success ml-2 btn-todo" 
-							onClick={e => this.setToDone(taskId, e)}
-						>
-							<i className="fas fa-check"/>
-						</button>
-						<button 
-							type="button" 
-							className="btn btn-outline-danger ml-2 btn-todo" 
-							onClick={e => this.remove(taskId, e)}
-						>
-							<i className="fas fa-trash"/>
-						</button>
-					</div>
-				</li>
-			)
-		});
-		return (
-				<div>
-					<ul className="todo-list list-group list-group-flush">
-						{items}
-					</ul>
-				</div>
-		)
-	}
+    render () {
+        const items = this.props.thingsToDo.map((el, i) => {
+            let taskId = "task" + i;
+            return (
+                <li key={i} id={taskId} className="list-group-item todo-item">
+                    <div className="todo-task">
+                        <span className="id">{i+1}. </span>
+                        <span className="title">{el.text}</span>
+                        <span className={`todo-${el.status} ml-2`}><i className="fas fa-check"/></span>
+                    </div>
+                    <div className="todo-buttons" id={taskId}>
+                        <button
+                            type="button" 
+                            className="btn btn-outline-success ml-2 btn-todo" 
+                            onClick={e => this.setToDone(taskId, e)}
+                        >
+                            <i className="fas fa-check"/>
+                        </button>
+                        <button 
+                            type="button" 
+                            className="btn btn-outline-danger ml-2 btn-todo" 
+                            onClick={e => this.remove(taskId, e)}
+                        >
+                            <i className="fas fa-trash"/>
+                        </button>
+                    </div>
+                </li>
+            )
+        });
+        return (
+                <div>
+                    <ul className="todo-list list-group list-group-flush">
+                        {items}
+                    </ul>
+                </div>
+        )
+    }
 }
