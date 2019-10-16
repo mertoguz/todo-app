cb61c0cf5db8b32d517729dbf23b63b8c9cf57a7
Mert Oğuz <mertoguz94@gmail.com>
2019-10-16 20:21:22 +0200

    fix styles, tab 4 spaces

--- src/App.js
+++ src/App.js
@@ -4,62 +4,65 @@ import {TodoForm} from './todo-form';
 
 class App extends Component {
 
-  constructor() {
-    super();
+	constructor() {
+		super();
 
-    this.state = { thingsToDo : [
-      { text: "Work hard", status: "todo" },
-      { text: "Play hard", status: "todo" },
-      { text: "Love more", status: "todo" }
-    ]};
+		this.state = { thingsToDo : [
+			{ text: "Work hard", status: "todo" },
+			{ text: "Play hard", status: "todo" },
+			{ text: "Love more", status: "todo" }
+		]};
 
-    this.setToDone = this.setToDone.bind(this);
-    this.remove = this.remove.bind(this);
-  }
+		this.setToDone = this.setToDone.bind(this);
+		this.remove = this.remove.bind(this);
+	}
   
-  addToList = (todo) =>  {
-    if(todo) {
-      let tempList = this.state.thingsToDo;
-      tempList.push({ text : todo, status: "todo" });
-      this.setState({thingsToDo : tempList})
-    }
-  }
+	addToList = (todo) =>  {
+		if(todo) {
+			let tempList = this.state.thingsToDo;
+			tempList.push({ text : todo, status: "todo" });
+			this.setState({thingsToDo : tempList})
+		}
+	}
 
-  setToDone (id) {
-    id = id.replace("task","");
-    let tempList = this.state.thingsToDo;
-    
-    //sometimes id is not coming I don't know wtf
-    if (id) {
-    tempList[id].status = "done";
-    this.setState({thingsToDo : tempList});
-    }
-  }
+	setToDone (id) {
+		id = id.replace("task","");
+		let tempList = this.state.thingsToDo;
 
-  remove (id) {
-    id = id.replace("task","");
-    let tempList = this.state.thingsToDo;
-    
-    if (id) {
-      tempList.splice(id, 1);
-      this.setState({thingsToDo : tempList});
-    }
-  }
+		//sometimes id is not coming I don't know wtf
+		if (id) {
+		tempList[id].status = "done";
+		this.setState({thingsToDo : tempList});
+		}
+	}
 
-  render() {
-    return (
-      <div>
-        <div className="header"><h1>Simple and lovely to do App</h1></div>
-        <div className="jumbotron content">
-          <TodoForm addToList={this.addToList}/>
-          <TodoList thingsToDo ={this.state.thingsToDo} 
-                    setToDone={this.setToDone} 
-                    remove={this.remove}
-          />
-      </div>
-     </div>
-    );
-  }
+	remove (id) {
+		id = id.replace("task","");
+		let tempList = this.state.thingsToDo;
+
+		if (id) {
+			tempList.splice(id, 1);
+			this.setState({thingsToDo : tempList});
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="header">
+					<h1>Simple and lovely to do App</h1>
+				</div>
+				<div className="jumbotron content">
+					<TodoForm addToList={this.addToList} />
+					<TodoList 
+						thingsToDo ={this.state.thingsToDo} 
+						setToDone={this.setToDone} 
+						remove={this.remove}
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
